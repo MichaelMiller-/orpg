b51b189e4f68cf55df03dd9ed6734622d54495c1
MichaelMiller- <miller.michael@gmx.de>
2016-11-23 15:59:07 +0100

    add test directory

---
+++ test/resource_manager.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <iostream>
+
+#include "inc/resource_manager.h"
+
+TEST_CASE("test case 1", "[resource_manager]")
+{
+   using namespace orpg;
+
+
+}
+
