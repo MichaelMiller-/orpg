9a45f204fdd3b3dd0c3c1df0c598200fd51b0db4
michael miller <miller.michael@gmx.de>
2022-04-16 12:24:42 +0200

    remove test entities

--- src/entities.h
+++ src/entities.h
@@ -3,10 +3,11 @@
 #include "entities/none.h"
 #include "entities/blocked.h"
 #include "entities/portal.h"
+#include "entities/chest.h"
 
 namespace orpg {
 
-    using entities_t = std::variant<none, chest, var2, blocked, portal>;
+    using entities_t = std::variant<none, blocked, chest, portal>;
 
     auto &operator<<(std::ostream &os, entities_t const &obj) {
         std::visit([&os](auto e) { os << e; }, obj);
