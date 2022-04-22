f07c3b9bb28adca8b33aa9ad1851505690a39dbc
michael miller <miller.michael@gmx.de>
2022-04-22 20:01:17 +0200

    add some tests

--- tests/point.cpp
+++ tests/point.cpp
@@ -2,15 +2,24 @@
 
 #include "src/point.h"
 
-TEST_CASE("add to points", "[geometry]")
+TEST_CASE("point", "[geometry]")
 {
     using namespace orpg;
 
     const point pt1{5, 5};
     const point pt2{2, 7};
 
-    const auto result = pt1 + pt2;
+    SECTION("compare")
+    {
+       REQUIRE(pt1 == point{5,5});
+       REQUIRE(pt1 != point{3,5});
+       REQUIRE(pt1 != point{5,2});
+    }
+    SECTION("addition")
+    {
+       const auto result = pt1 + pt2;
 
-    REQUIRE(result.x == 7);
-    REQUIRE(result.y == 12);
+       REQUIRE(result.x == 7);
+       REQUIRE(result.y == 12);
+    }
 }
